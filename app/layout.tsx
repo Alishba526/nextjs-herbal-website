6210f1f5f90b3d37ab1c230394cf72de572dd21e
Alishba Rehman <159262026+Alishba526@users.noreply.github.com>
2026-05-16 01:59:59 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
       <head>
        <link rel="icon" href="/assets/logo.png" />
        <link rel="apple-touch-icon" href="/assets/logo.png" />
+       <meta name="google-site-verification" content="N-dQ4BCgU5LJhbscIMPh5D7E0oIMpFAoIXKuMkDWtpc" />
       </head>
       <body className={`${inter.variable} ${playfair.variable} antialiased`}>
         <Navbar />
